1d5276800a3a2717964984b1f13cf6365cc3ba2b
Evan <evanahouse@gmail.com>
2021-05-10 17:08:25 -0700

    created basic skeleten on app, fetching from api, a little css, deleted some of the basic template from create-react-app

---
+++ src/components/ShowCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class ShowCard extends Component {
+    render() {
+        return (
+            <div className="main-show">
+                <button onClick={(e) => this.props.hide(e)}>x</button>
+                <div className="left-show">
+                    <img src={this.props.movie.image_url}></img>
+                    <h2>{this.props.movie.title} ({this.props.movie.release_date}) </h2>
+                </div>
+                <div className="right-show">
+                    <p>{this.props.movie.original_title} "{this.props.movie.original_title_romanized}"</p>
+                    <p>Director: {this.props.movie.director}</p>
+                    <p>Producer: {this.props.movie.producer}</p>
+                    <p>Runtime: {this.props.movie.run_time} minutes</p>
+                    <p>Rotten Tomatoes: {this.props.movie.rt_score}%</p>
+                    <p>Description: {this.props.movie.description}</p>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/pages/Show.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ShowCard from '../components/ShowCard'
+
+export default class Show extends Component {
+    render() {
+        return (
+            <div>
+                <ShowCard movie={this.props.movie}/>
+            </div>
+        )
+    }
+}
